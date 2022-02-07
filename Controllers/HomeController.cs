ae2c65a2bdf4a67ab343c5b9af29ac6831b4edc5
markdvogt <90359765+markdvogt@users.noreply.github.com>
2022-02-07 09:10:30 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project_1_prac.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_1_prac.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
